6c33e6ac9d4da1e55a2d94fcbeaf196ab3819d21
inconnu000 <inconnu000@DESKTOP-77ULMH9.localdomain>
2018-09-26 17:53:29 +0200

    adding yarn

--- src/Repository/FidgetRepository.php
+++ src/Repository/FidgetRepository.php
@@ -17,7 +17,7 @@ class FidgetRepository
         $fidget1->setSide(3);
         $fidget1->setColor("red");
         $fidget1->setShape("classic");
-        $fidget1->setPic("/img/classic.jpg");
+        $fidget1->setPic("/img/classic.png");
 
         $fidget2 = new Fidget();
         $fidget2->setId(2);
@@ -25,7 +25,7 @@ class FidgetRepository
         $fidget2->setSide(6);
         $fidget2->setColor("rainbow");
         $fidget2->setShape("atom");
-        $fidget2->setPic("/img/atom.jpg");
+        $fidget2->setPic("/img/atom.png");
 
         $fidget3 = new Fidget();
         $fidget3->setId(3);
@@ -33,7 +33,7 @@ class FidgetRepository
         $fidget3->setSide(3);
         $fidget3->setColor("rainbow");
         $fidget3->setShape("wing");
-        $fidget3->setPic("/img/dragon.jpg");
+        $fidget3->setPic("/img/dragon.png");
 
         $fidget4 = new Fidget();
         $fidget4->setId(4);
@@ -41,7 +41,7 @@ class FidgetRepository
         $fidget4->setSide(3);
         $fidget4->setColor("rainbow");
         $fidget4->setShape("classic");
-        $fidget4->setPic("/img/rainbow.jpg");
+        $fidget4->setPic("/img/rainbow.png");
         //</editor-fold>
 
         //<editor-fold desc="objects list">
